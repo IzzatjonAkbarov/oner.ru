04c688ae8e57b4364b69bbbe667665161b2adc83
IzzatjonAkbarov <akbrv.i.006@gmail.com>
2025-02-12 12:00:16 +0500

    some chnages added

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -3,7 +3,7 @@ import Home from "../pages/Home";
 import MainLayout from "../components/MainLayout";
 import Contacts from "../pages/Contacts";
 import Payment from "../pages/Payment";
-import Delivery from "../pages/delivery";
+import Delivery from "../pages/Delivery";
 import Cart from "../pages/Cart";
 
 export const rooter = createBrowserRouter([
